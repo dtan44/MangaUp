4ca78ba9a9c4b4f1881035fe3ab8fd3ae0202516
dtan44 <dtan444@gmail.com>
2017-08-28 18:33:58 -0400

    simple server

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 // MangaUp index
 
 const express = require('express')
+const app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+app.get('/', function(request, response) {
+    response.send('Hello World')
+})
+
+app.listen(app.get('port'), function(err) {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+    console.log('Node app is running on port', app.get('port'))
+})
