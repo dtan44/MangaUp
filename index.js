f2a5dcb3b6f9d94130deb0d784d283f250d6f1e3
dtan44 <dtan444@gmail.com>
2017-08-28 18:51:51 -0400

    validation test

--- index.js
+++ index.js
@@ -2,12 +2,24 @@
 
 const express = require('express')
 const app = express()
+const verifytkn = "thisisme"
 
 app.set('port', (process.env.PORT || 5000))
 
 app.get('/', function(request, response) {
     console.log(request.url)
     response.send('Hello World')
+})
+
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === verifytkn) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
 })
 
 app.listen(app.get('port'), function(err) {
